b55db39b3b89c7bcd729710f100a16e4080324f5
dzhygrynyuk <dzhygrynyuk@gmail.com>
2023-08-10 00:35:35 +0300

    RegisterForm: created success window

--- src/modules/RegisterForm/components/RegisterForm.jsx
+++ src/modules/RegisterForm/components/RegisterForm.jsx
@@ -1,13 +1,14 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-import { LockOutlined, UserOutlined, MailOutlined } from '@ant-design/icons';
+import { LockOutlined, UserOutlined, MailOutlined, ExclamationCircleOutlined } from '@ant-design/icons';
 import { Form, Input } from "antd";
 
 import { Button, Block } from "components";
 
 class RegisterForm extends React.Component {
     render() {
+        const success = false;
         return (
             <div>
                 <div className="auth__top">
@@ -15,50 +16,60 @@ class RegisterForm extends React.Component {
                     <p>Please enter your personal data!</p>
                 </div>
                 <Block>
-                    <Form className="login-form">
-                        <Form.Item>
-                            <Input
-                                prefix={
-                                    <UserOutlined className="site-form-item-icon" style={{ color: '#adadad' }} />
-                                }
-                                size="large"
-                                placeholder="Username"
-                            />
-                        </Form.Item>
-                        <Form.Item>
-                            <Input
-                                prefix={
-                                    <MailOutlined className="site-form-item-icon" style={{ color: '#adadad' }} />
-                                }
-                                size="large"
-                                placeholder="Email"
-                            />
-                        </Form.Item>
-                        <Form.Item>
-                            <Input
-                                prefix={
-                                    <LockOutlined className="site-form-item-icon" style={{ color: '#adadad' }} />
-                                }
-                                size="large"
-                                type="password"
-                                placeholder="Password"
-                            />
-                        </Form.Item>
-                        <Form.Item>
-                            <Input
-                                prefix={
-                                    <LockOutlined className="site-form-item-icon" style={{ color: '#adadad' }} />
-                                }
-                                size="large"
-                                type="password"
-                                placeholder="Confirm Password"
-                            />
-                        </Form.Item>
-                        <Form.Item>
-                            <Button type="primary" size="large">Registration</Button>
-                        </Form.Item>
-                        <Link className="auth__register-link" to="/login">Login</Link>
-                    </Form>
+                    {!success ? (
+                        <Form className="login-form">
+                            <Form.Item>
+                                <Input
+                                    prefix={
+                                        <UserOutlined className="site-form-item-icon" style={{ color: '#adadad' }} />
+                                    }
+                                    size="large"
+                                    placeholder="Username"
+                                />
+                            </Form.Item>
+                            <Form.Item>
+                                <Input
+                                    prefix={
+                                        <MailOutlined className="site-form-item-icon" style={{ color: '#adadad' }} />
+                                    }
+                                    size="large"
+                                    placeholder="Email"
+                                />
+                            </Form.Item>
+                            <Form.Item>
+                                <Input
+                                    prefix={
+                                        <LockOutlined className="site-form-item-icon" style={{ color: '#adadad' }} />
+                                    }
+                                    size="large"
+                                    type="password"
+                                    placeholder="Password"
+                                />
+                            </Form.Item>
+                            <Form.Item>
+                                <Input
+                                    prefix={
+                                        <LockOutlined className="site-form-item-icon" style={{ color: '#adadad' }} />
+                                    }
+                                    size="large"
+                                    type="password"
+                                    placeholder="Confirm Password"
+                                />
+                            </Form.Item>
+                            <Form.Item>
+                                <Button type="primary" size="large">Registration</Button>
+                            </Form.Item>
+                            <Link className="auth__register-link" to="/login">Login</Link>
+                        </Form>
+                    ) : (
+                        <div className="auth__success-block">
+                            <div>
+                                <ExclamationCircleOutlined className="site-form-item-icon" style={{ color: '#177CFA' }} />
+                            </div>
+                            <h2>Confirm your account</h2>
+                            <p>You have been sent an email with a link to confirm your account.</p>
+                        </div>
+                    )}
                 </Block>
             </div>
         );
